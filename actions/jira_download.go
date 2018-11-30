22a56b4ed0ac290e552ad748dda3f9e3fd7c21b5
MrFlynn <nick@pleatsikas.me>
2018-11-29 20:56:30 -0800

    Migrated JIRA backup code to use new request and download methods.

--- actions/jira_download.go
+++ actions/jira_download.go
@@ -1,14 +1,9 @@
 package actions
 
 import (
-	"errors"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
 	"os"
 
-	"github.com/fatih/color"
 	"github.com/midN/jira-cloud-backuper/common"
 	"gopkg.in/urfave/cli.v1"
 )
@@ -27,23 +22,18 @@ func JiraDownload() func(c *cli.Context) error {
 		}
 		defer out.Close()
 
-		client, host, err := common.AuthUser(c)
+		latestID, err := latestJiraTaskID(c)
 		if err != nil {
 			return common.CliError(err)
 		}
 
-		latestID, err := latestJiraTaskID(client, host)
-		if err != nil {
-			return common.CliError(err)
-		}
-
-		downloadURL, err := common.JiraWaitForBackupReadyness(client, latestID, host)
+		downloadURL, err := common.JiraWaitForBackupReadyness(c, latestID)
 		if err != nil {
 			return common.CliError(err)
 		}
 
 		fmt.Println("Downloading to", filename)
-		result, err := downloadLatestJira(client, downloadURL, out)
+		result, err := common.DownloadFile(c, downloadURL, out)
 		if err != nil {
 			return common.CliError(err)
 		}
@@ -53,31 +43,11 @@ func JiraDownload() func(c *cli.Context) error {
 	}
 }
 
-func latestJiraTaskID(client http.Client, host string) (string, error) {
-	url := host + "/rest/backup/1/export/lastTaskId"
-	resp, _ := client.Get(url)
-	body, _ := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return "", errors.New(string(body))
-	}
-	return string(body), nil
-}
-
-func downloadLatestJira(client http.Client, url string, out *os.File) (string, error) {
-	resp, _ := client.Get(url)
-	if resp.StatusCode == 404 {
-		return "", errors.New("File not found at " + url)
-	}
-	defer resp.Body.Close()
-
-	readerpt := &common.PassThru{Reader: resp.Body, Length: resp.ContentLength}
-	count, err := io.Copy(out, readerpt)
+func latestJiraTaskID(c *cli.Context) (string, error) {
+	body, err := common.DoRequest(c, "GET", "/rest/backup/1/export/lastTaskId", map[string]string{}, nil)
 	if err != nil {
-		return "", err
+		return "", nil
 	}
 
-	return color.GreenString(fmt.Sprintln(
-		"Download finished, file size:", count, "bytes.", "File:", out.Name())), nil
+	return string(body), nil
 }
--- common/jira.go
+++ common/jira.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"time"
+
+	"gopkg.in/urfave/cli.v1"
 )
 
 type jiraProgressResponse struct {
@@ -18,12 +18,12 @@ type jiraProgressResponse struct {
 
 // JiraWaitForBackupReadyness check status of a backup
 // and loops until it's ready
-func JiraWaitForBackupReadyness(client http.Client, id string, host string) (string, error) {
+func JiraWaitForBackupReadyness(c *cli.Context, id string) (string, error) {
 	downloadURL, progress := "", 0
 	var err error
 
 	for progress < 100 {
-		downloadURL, progress, err = jiraCheckBackupProgress(client, id, host)
+		downloadURL, progress, err = jiraCheckBackupProgress(c, id)
 		if err != nil {
 			return downloadURL, err
 		}
@@ -39,27 +39,28 @@ func JiraWaitForBackupReadyness(client http.Client, id string, host string) (str
 	return downloadURL, nil
 }
 
-func jiraCheckBackupProgress(client http.Client, id string, host string) (string, int, error) {
-	var respJSON = new(jiraProgressResponse)
-	url := host + "/rest/backup/1/export/getProgress?taskId=" + id
-	resp, _ := client.Get(url)
-	body, _ := ioutil.ReadAll(resp.Body)
-	json.Unmarshal(body, &respJSON)
-	resp.Body.Close()
+func jiraCheckBackupProgress(c *cli.Context, id string) (string, int, error) {
+	respJSON := jiraProgressResponse{}
 
-	if resp.StatusCode != 200 {
-		return "", 0, errors.New(string(body))
+	// Do request and handle any errors.
+	body, err := DoRequest(c, "GET", "/rest/backup/1/export/getProgress?taskId="+id, map[string]string{}, nil)
+	if err != nil {
+		return "", 0, err
 	}
 
+	json.Unmarshal(body, &respJSON)
+
+	// Check to make sure that the job hasn't failed before returning URL.
 	if respJSON.Status == "Failed" {
 		return "", 0, errors.New(respJSON.Result)
 	}
 
-	return jiraDownloadURL(respJSON.Result, host), respJSON.Progress, nil
+	// Return download path and progress percent.
+	return jiraDownloadURL(respJSON.Result), respJSON.Progress, nil
 }
 
-func jiraDownloadURL(path string, host string) string {
-	url := host + "/plugins/servlet/" + path
+func jiraDownloadURL(path string) string {
+	url := "/plugins/servlet/" + path
 
 	return url
 }
